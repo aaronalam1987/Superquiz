60d3f18551ba717ce7a76a255247f9f25f93cf17
Aaron Alam <aaronalam1987@outlook.com>
2022-10-30 18:49:14 +0000

    changed the way questions are stored and commited them to program memory to save ram

--- Superquiz.ino
+++ Superquiz.ino
@@ -5,7 +5,7 @@ LiquidCrystal_I2C lcd2 (0x26, 16, 2); //Define 16x2 LCD
 int buttonPressed = 0; //int for which button is pressed.
 int welcome = 0; //Set welcome to 0 to display welcome message.
 int questionNum = 0; //Define question number.
-int maxQuestion = 10;
+int totalQuestions = 10; //Set question amount.
 int score = 0; //Keep score.
 
 void setup() {
@@ -46,7 +46,14 @@ void doDisplay(String L1, String L2, String L3, String L4, String L5, String L6,
                 lcd2.print(L8);
             
 }
-
+void doEnd(){
+ lcd.setCursor(0,0);
+    lcd.print("Game over");
+      lcd.setCursor(0,1);
+        lcd.print("Your score is:");
+          lcd.setCursor(0,2);
+            lcd.print(score);
+}
 //Function for if answer is correct.
 void doAnswer(int a){
       lcd.clear(); //Clear lcd one
@@ -69,39 +76,44 @@ void doAnswer(int a){
 
 //Question struct.
 struct Question {
-String line1; 
-String line2;
-String line3;
-String line4;
-String line5;
-String line6;
-String line7;
-String line8;
-int ANS; 
+const String line1; 
+const String line2;
+const String line3;
+const String line4;
+const String line5;
+const String line6;
+const String line7;
+const String line8;
+const int ANS; 
 };
 
-//Define question and answers, LCD one (line 1,2,3,4) LCD2 (line 1 (button 1), line 1.7 (button 2), line 2 (button 3), line 2.7 (button 4)) button tied to answer.
-Question welcomeMess = {"Welcome to", "Superquiz", "Press any button", "to continue", "Ready", "to", "play", "?", 1}; 
-Question Q1 = {"How many", "seasons of", "Friends", "were there?", "1", "4", "6", "9", 4};
-Question Q2 = {"Who pees on", "monica", "after she is", "stung by a jelly fish?", "Chandler", "Rachel", "Joey", "Pheobe", 3};
-Question Q3 = {"Ross says whos", "name at the altar", "in london?", "", "Rachel", "Pheobe", "Monica", "Emily", 1};
-
-
+const char * const welcomeMess[9] PROGMEM = { "Welcome to", "Superquiz", "press any button", "to continue.", "Ready", "to", "play", "?", 1 };
+const char * const gameEnd[3] PROGMEM = { "Game Over", "Your score is:", 1 };
+const char * const Q1[9] PROGMEM = { "How many", "seasons of", "Friends", "where there?", "1", "4", "6", "9", 4 };
+const char * const Q2[9] PROGMEM = { "Who pees on", "Monica", "after she is", "stung by a jelly fish?", "Chandler", "Rachel", "Joey", "Pheobe", 3 };
+const char * const Q3[9] PROGMEM = { "Ross says whos", "name at the altar", "in london?", "", "Rachel", "Pheobe", "Monica", "Emily", 1 };
+const char * const Q4[9] PROGMEM = { "How many sisters", "does joey have?", "", "", "5", "7", "2", "0", 2 };
+const char * const Q5[9] PROGMEM = { "What is the name of", "Joeys character in", "the commercial for", "opening milk cartons?", "Mike", "Tommy", "Kevin", "Drake", 3 };
+const char * const Q6[9] PROGMEM = { "What instrument did", "  Ross intend to play", "at Moncica and", "Chandlers Wedding?", "Keyboard", "Drums", "Guitar", "Sax", 1 };
+const char * const Q7[9] PROGMEM = { "What is Chandlers", "middle name?", "", "", "Nora", "Muiel", "Charles", "Francis", 2 };
+const char * const Q8[9] PROGMEM = { "Who was", "Monicas first kiss?", "", "", "Chandler", "Pete", "Ross", "Richard", 3 };
+const char * const Q9[9] PROGMEM = { "Chick Jr. and Duck Jr.", "get stuck in what?", "", "", "Toilet", "Foosball", "Bin", "Fridge", 2 }; 
+const char * const Q10[9] PROGMEM = { "Which famous character", "famously said", "PIVOT?", "", "Rachel", "Joey", "Monica", "Ross", 4 };
 
 void doButtons(){
 switch (questionNum){
   case 0:
-  if(buttonPressed > 0){
+  if(buttonPressed > 0){ //If any button pressed.
       welcome=1; //Set welcome to 1 to show we have passed this stage.
       questionNum++; //Set first question
-         lcd.clear(); //Clear lcd one.
-          lcd2.clear(); //Clear lcd two.
-            buttonPressed=0; //Reset button press.
-              delay(1000); //Delay for one second.
-    }
+      lcd.clear(); //Clear lcd one.
+      lcd2.clear(); //Clear lcd two.
+      buttonPressed=0; //Reset button press.
+      delay(1000); //Delay for one second.
+  }
   case 1: 
   if(buttonPressed > 0){ //As we reset button to 0, make sure it is more than 0.
-        if(buttonPressed==Q1.ANS){ 
+        if(buttonPressed==Q1[8]){ 
             doAnswer(1); //Call answerCorrect function.  
         }
         else{
@@ -110,7 +122,7 @@ switch (questionNum){
   }
   case 2:
   if(buttonPressed > 0){
-        if(buttonPressed==Q2.ANS){
+        if(buttonPressed==Q2[8]){
             doAnswer(1);       
         }
         else{
@@ -119,13 +131,77 @@ switch (questionNum){
   }
   case 3:
   if(buttonPressed > 0){
-        if(buttonPressed==Q3.ANS){
+        if(buttonPressed==Q3[8]){
             doAnswer(1); 
         }
         else{
            doAnswer(0);
         }
   }
+  case 4:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q4[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 5:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q5[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 6:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q6[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 7:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q7[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 8:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q8[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 9:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q9[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  case 10:
+  if(buttonPressed > 0){
+        if(buttonPressed==Q10[8]){
+            doAnswer(1); 
+        }
+        else{
+           doAnswer(0);
+        }
+  }
+  
 }
 
 
@@ -133,20 +209,51 @@ switch (questionNum){
 
 void loop() {
   if(welcome==0){ //Display welcome message.
-    doDisplay(welcomeMess.line1, welcomeMess.line2, welcomeMess.line3, welcomeMess.line4, welcomeMess.line5, welcomeMess.line6, welcomeMess.line7, welcomeMess.line8);
+    doDisplay(welcomeMess[0], welcomeMess[1], welcomeMess[2], welcomeMess[3], welcomeMess[4], welcomeMess[5], welcomeMess[6], welcomeMess[7]);
     doButtons();
   }
   else{
+    if(questionNum > totalQuestions){ //Check which question is active and display it.
+      doEnd();
+    }
     if(questionNum==1){ //Check which question is active and display it.
-      doDisplay(Q1.line1, Q1.line2, Q1.line3, Q1.line4, Q1.line5, Q1.line6, Q1.line7, Q1.line8);
+      doDisplay(Q1[0], Q1[1], Q1[2], Q1[3], Q1[4], Q1[5], Q1[6], Q1[7]);
       doButtons();
     }
     if(questionNum==2){
-      doDisplay(Q2.line1, Q2.line2, Q2.line3, Q2.line4, Q2.line5, Q2.line6, Q2.line7, Q2.line8);
+      doDisplay(Q2[0], Q2[1], Q2[2], Q2[3], Q2[4], Q2[5], Q2[6], Q2[7]);
       doButtons();
     }
     if(questionNum==3){
-      doDisplay(Q3.line1, Q3.line2, Q3.line3, Q3.line4, Q3.line5, Q3.line6, Q3.line7, Q3.line8);
+      doDisplay(Q3[0], Q3[1], Q3[2], Q3[3], Q3[4], Q3[5], Q3[6], Q3[7]);
+      doButtons();
+    }
+    if(questionNum==4){
+      doDisplay(Q4[0], Q4[1], Q4[2], Q4[3], Q4[4], Q4[5], Q4[6], Q4[7]);
+      doButtons();
+    }
+    if(questionNum==5){
+      doDisplay(Q5[0], Q5[1], Q5[2], Q5[3], Q5[4], Q5[5], Q5[6], Q5[7]);
+      doButtons();
+    }
+    if(questionNum==6){
+      doDisplay(Q6[0], Q6[1], Q6[2], Q6[3], Q6[4], Q6[5], Q6[6], Q6[7]);
+      doButtons();
+    }
+    if(questionNum==7){
+      doDisplay(Q7[0], Q7[1], Q7[2], Q7[3], Q7[4], Q7[5], Q7[6], Q7[7]);
+      doButtons();
+    }
+    if(questionNum==8){
+      doDisplay(Q8[0], Q8[1], Q8[2], Q8[3], Q8[4], Q8[5], Q8[6], Q8[7]);
+      doButtons();
+    }
+    if(questionNum==9){
+      doDisplay(Q9[0], Q9[1], Q9[2], Q9[3], Q9[4], Q9[5], Q9[6], Q9[7]);
+      doButtons();
+    }
+    if(questionNum==10){
+      doDisplay(Q10[0], Q10[1], Q10[2], Q10[3], Q10[4], Q10[5], Q10[6], Q10[7]);
       doButtons();
     }
   }
